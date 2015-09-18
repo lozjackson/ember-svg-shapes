f527e0fc44ba1b0b8e92e0086d414b1241a308a6
Loz Jackson <lozjackson@talktalk.net>
2015-09-18 18:44:03 +0100

    update dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -25,6 +25,10 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.contentSecurityPolicy = {
+      'style-src': "'self' 'unsafe-inline'"
+    }
   }
 
   if (environment === 'test') {
