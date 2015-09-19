a77484d756bb14a18e73194724e8eaab7b13609b
Loz Jackson <lozjackson@talktalk.net>
2015-09-19 22:39:51 +0100

    update dummy app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,9 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    rotateStar: 0,
-
-    _rotateStar: Ember.computed('rotateStar', function() {
-        return (this.get('rotateStar') * 3.6).toFixed(1);
-    }),
+    rotateStar: 0
 });
