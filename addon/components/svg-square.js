edd2be43afe0916e05182d0426a9cef6c7b61ea2
Loz Jackson <lozjackson@talktalk.net>
2015-09-19 19:49:58 +0100

    update svg-square component

--- addon/components/svg-square.js
+++ addon/components/svg-square.js
@@ -1,6 +1,84 @@
+/**
+  @module ember-svg-shapes
+*/
 import Ember from 'ember';
+import SvgMixin from 'ember-svg-shapes/mixins/svg';
 import layout from '../templates/components/svg-square';
 
-export default Ember.Component.extend({
-  layout: layout
+/**
+  @class SvgSquareComponent
+  @namespace SvgShapes
+  @uses SvgShapes.SvgMixin
+*/
+export default Ember.Component.extend( SvgMixin, {
+
+    layout: layout,
+
+    /**
+      @property className
+      @type {Array}
+      @default ['svg-square']
+      @private
+    */
+    classNames: ['svg-square'],
+
+    /**
+      @property radiusX
+      @type {Number}
+      @default 0
+    */
+    radiusX: 0,
+
+    /**
+      @property radiusX
+      @type {Number}
+      @default 0
+    */
+    radiusY: 0,
+
+    /**
+      @property size
+      @type {Number}
+      @default 10
+    */
+    size: 10,
+
+    /**
+      @property _height
+      @type {Number}
+      @readonly
+      @private
+    */
+    _height: Ember.computed('size', 'strokeWidth', function() {
+        var size= this.get('size'),
+            strokeWidth = this.get('strokeWidth');
+        return size - strokeWidth;
+    }),
+
+    /**
+      @property _width
+      @type {Number}
+      @readonly
+      @private
+    */
+    _width: Ember.computed.alias('_height'),
+
+    /**
+      @property left
+      @type {Number}
+      @readonly
+      @private
+    */
+    left: Ember.computed('strokeWidth', function() {
+        var strokeWidth = this.get('strokeWidth');
+        return strokeWidth / 2;
+    }),
+
+    /**
+      @property top
+      @type {Number}
+      @readonly
+      @private
+    */
+    top: Ember.computed.alias('left')
 });
