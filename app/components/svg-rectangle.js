b708244acdb4c58b1f4a1cae49b2f701bdb89c4a
Loz Jackson <lozjackson@talktalk.net>
2016-02-24 16:42:22 +0000

    add gh-pages branch

--- app/components/svg-rectangle.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-svg-shapes/components/svg-rectangle';
--- tests/dummy/app/controllers/svg-circle.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-
-    size: 200,
-
-    strokeWidth: 10
-});
