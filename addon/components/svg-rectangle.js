80db361750a87608c11dbf1b5ed39387a497d6f6
Loz Jackson <lozjackson@talktalk.net>
2016-03-26 17:41:17 +0000

    update components

--- addon/components/svg-rectangle.js
+++ addon/components/svg-rectangle.js
@@ -4,6 +4,7 @@
 import Ember from 'ember';
 import SvgMixin from 'ember-svg-shapes/mixins/svg';
 import RectMixin from 'ember-svg-shapes/mixins/rect';
+import FilterMixin from 'ember-svg-shapes/mixins/filter';
 import layout from '../templates/components/svg-rectangle';
 
 /**
@@ -11,8 +12,9 @@ import layout from '../templates/components/svg-rectangle';
   @namespace SvgShapes.Components
   @uses SvgShapes.Mixins.SvgMixin
   @uses SvgShapes.Mixins.RectMixin
+  @uses SvgShapes.Mixins.FilterMixin
 */
-export default Ember.Component.extend(SvgMixin, RectMixin, {
+export default Ember.Component.extend(SvgMixin, RectMixin, FilterMixin, {
 
   layout,
 
--- addon/components/svg-square.js
+++ addon/components/svg-square.js
@@ -4,6 +4,7 @@
 import Ember from 'ember';
 import SvgMixin from 'ember-svg-shapes/mixins/svg';
 import RectMixin from 'ember-svg-shapes/mixins/rect';
+import FilterMixin from 'ember-svg-shapes/mixins/filter';
 import layout from '../templates/components/svg-square';
 
 /**
@@ -11,8 +12,9 @@ import layout from '../templates/components/svg-square';
   @namespace SvgShapes.Components
   @uses SvgShapes.Mixins.SvgMixin
   @uses SvgShapes.Mixins.RectMixin
+  @uses SvgShapes.Mixins.FilterMixin
 */
-export default Ember.Component.extend(SvgMixin,  RectMixin, {
+export default Ember.Component.extend(SvgMixin,  RectMixin, FilterMixin, {
 
   layout,
 
