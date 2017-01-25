7013c2a0f1f579ef06b730767efb2eda47b6ea4a
Loz Jackson <lozjackson@talktalk.net>
2017-01-25 20:00:02 +0000

    update tests

--- tests/unit/components/svg-rectangle-test.js
+++ tests/unit/components/svg-rectangle-test.js
@@ -1,7 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('svg-rectangle', 'Unit | Component | svg rectangle', {
-  // needs: [],
   unit: true
 });
 
@@ -18,11 +17,4 @@ test('tagName', function(assert) {
   var component = this.subject();
   this.render();
   assert.equal(component.get('tagName'), 'svg');
-});
-
-test('classNames', function(assert) {
-  assert.expect(1);
-  var component = this.subject();
-  this.render();
-  assert.deepEqual(component.get('classNames'), ['ember-view', 'ember-svg-shapes', 'svg-rectangle']);
 });
